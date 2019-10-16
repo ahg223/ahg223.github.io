024c6289922d47c71c9dfbcf6147a3897b28fae7
ahg223 <ahg2230@gmail.com>
2019-10-17 00:18:18 +0900

    Restart by 404

---
+++ _pages/about.md
@@ -0,0 +1,12 @@
+---
+title: "이 블로그 설명서"
+permalink: /about/
+layout: single
+---
+
+## ahg223 blog
+
+컴퓨터 교육과를 왔는데 컴퓨터가 너무 재밌네요.
+세상 신기한게 많아 공부하고 기록하려 만든 블로그입니다.
+
+<https://ahg223.com>
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "coder vs programmer",
+        "excerpt":"공학이란 순수 과학과 달리 현실적 문제 해결이 중요하다. 현실적 문제 해결이란 일상 속 Fail point 발견부터 문제에 내제된 의미를 파악하여 최적화 된 해결과 가치를 부여하는 것 까지 포함된다. 순수과학과 공학은 이러한 점이 달라 보다 기술을 현실로 이어준다는 점을 공학도라면 알아야 한다. 나는 기술을 대하는 이 마음가짐이 코더와 프로그래머를 가르는 시작점이라...","categories": ["Private"],
+        "tags": ["Private","ahg223"],
+        "url": "http://localhost:4000/private/private/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "AI 레시피 프린터 - Intro",
+        "excerpt":"Code : https://github.com/ahg223/RecipePrinter_GCP Video : https://youtu.be/KZbDgwNwYfQ 프로젝트 기획 계기 및 의도 자동화의 가치 클라우드의 가치 AI의 가치 프로젝트 중 배운 것 생각보다 컴퓨터공학을 위한 시장은 넓다 첨단 기술이 오히려 경량화 된 서비스에 적합한 경우도 많다 로그데이터는 에러핸들링을 위해서만 사용되지 않는다. 그 중요함과 가치 프로젝트 에러 핸들링 및 피드백 보다 많은...","categories": ["ToyProject"],
+        "tags": ["ToyProject","AI","GCP","Intro"],
+        "url": "http://localhost:4000/toyproject/AIRecipePrinter_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "Study - PS",
+        "excerpt":"code : https://github.com/ahg223/ProblemSolve   boj, SW master 등 여러 ps 문제 공부 중 시간이 여유로울 때 풀이 업로드 예정   글 제목: Study - PS 수정 시간: 2019-09-09 16:51:00 +0000  ","categories": ["Study"],
+        "tags": ["Algorithm"],
+        "url": "http://localhost:4000/study/Algorithm/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "점자악보 번역기 - Intro",
+        "excerpt":"code : https://github.com/ahg223/BraileCompiler video : https://www.youtube.com/watch?v=EdedV2tOndw&amp;t=1s 프로젝트 기획 계기 및 의도 내가 장애인이었기에 이런 기획을 하게 되었다. 군대에서 그런 일이 있던 뒤, 꼭 나가서 타인을 위한 프로젝트를 진행하자 를 첫번째 목표로 세웠고, 이 프로젝트가 그것이었다. 프로젝트 중 배운 것 Git을 통한 개발자의 협업 문화에 대하여 배웠다. contribute와 pull이라는 것을 통해...","categories": ["ToyProject"],
+        "tags": ["ToyProject","Compiler","Intro"],
+        "url": "http://localhost:4000/toyproject/BraileCompiler_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "GG<3 - Intro",
+        "excerpt":"code : https://github.com/ahg223/DeepLeague_Data_Creator 프로젝트 기획 계기 및 의도 게이머에 대한 인식을 바꾸기 위해서, 사회의 인식을 바꾸기 위해서 현실과 가장 밀접한 컴퓨터 공학만이 할 수 있는 일이라고 생각 IT의 발전이 사회를 공포로 몰아간다는 뉘앙스를 영화 혹은 매체에서 자주 접할 수 있는데, 그만큼 사회 전반과 밀접하기 때문이다. 그 역으로 당연히 누구보다 높은...","categories": ["Software Maestro"],
+        "tags": ["Software Maestro","GCP","AWS","Mongo DB","AI","Big Data","Intro"],
+        "url": "http://localhost:4000/software%20maestro/GGHeart_Intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "노약자를 위한 키오스크 - Intro",
+        "excerpt":"code : https://github.com/ahg223/VoiceKiosk video : https://www.youtube.com/watch?v=aPoqsnse4nA&amp;t=5s https://www.youtube.com/watch?v=KKF8_7Jx1TQ 프로젝트 기획 계기 및 의도 “기술의 빠른 발전과 도입이 소외계층을 소외시킨다”를 부정하고 싶었다. 첨단 기술을 UX를 고려하여 도입한다면 그들도 소외되지 않는다. 유튜브에서 우연히 박막례 할머니가 햄버거가 먹고싶어 패스트푸드 점으로 갔지만, 키오스크 때문에 그냥 나오는 것을 보았다 인턴 때 공부한 VUX가 도움이 되는 상황이므로...","categories": ["ToyProject"],
+        "tags": ["ToyProject","Alexa","AWS","OpenSource","Intro"],
+        "url": "http://localhost:4000/toyproject/KioskProjector_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "대학 프로젝트 인력시장 - Intro",
+        "excerpt":"video : https://cheonuk.wixsite.com/danmyeong https://www.youtube.com/watch?v=LEAAoj7ToP0&amp;t=299s 프로젝트 기획 계기 및 의도 컴퓨터 공학의 본질은 통신, 자동화 그리고 데이터다. 2학기가 시작하자마자 여러 발표 준비를 하면서, 발표가 끝나면 아무도 이 자료를 신경도 안쓴다는 사실이 허무했다. 또한 직전 학기에 대외활동을 경험하자 보다 다양한 사람들과 개발 프로젝트를 진행하고 싶은 마음도 생겼다. 그렇다면 관련 전공을 가진 사람들끼리...","categories": ["ToyProject"],
+        "tags": ["ToyProject","Platform","Web","Database","Intro"],
+        "url": "http://localhost:4000/toyproject/Project-member-collect_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "SKT Intern",
+        "excerpt":"프로젝트 기획 계기 및 의도 인턴 최종 과제로 “Next Device - 20대를 위한 NUGU”를 기획해오라는 과제를 받음 5명의 인턴이 팀을 구성했는데, 디자인 대학원생, 경영, 컴공, 컴교 대학생으로 굉장히 다양했음 네명의 전공과 각각의 기획 방식이 전부 달랐기에 불협화음이 꽤 있었다 제품 기획은 디자인의 접근이 맞다고 생각해 크리에이티브 띵킹을 메인으로, 가능성 확인에서...","categories": ["Private"],
+        "tags": ["Intern","AI","UX","Intro"],
+        "url": "http://localhost:4000/private/SKTintern_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "자바 학습을 위한 슈팅게임 - Intro",
+        "excerpt":"video : https://www.youtube.com/watch?v=paIpaJ5Geao code : https://github.com/ahg223/MiniGame   객체지향 프로그래밍을 공부하기 위해 진행함   글 제목: 자바 학습을 위한 슈팅게임 - Intro 수정 시간: 2019-09-09 16:51:00 +0000  ","categories": ["ToyProject"],
+        "tags": ["ToyProject","Java","Intro"],
+        "url": "http://localhost:4000/toyproject/ShootingGame_intro/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "군복무 후기",
+        "excerpt":"평범한 6개월의 발전병과, 실청 후 6개월의 입원 조용히 책을 읽고 전공 공부를 하려고 공군 지원 후 노력해 발전병으로 특기 배정 과한 스트레스 때문에 실청 뒤 200일 입원 입원 중 많은 생각을 했고, 전역한 뒤 꼭 사회를 위한 프로젝트를 진행해보자 각오함 복귀 후 BX로 보직 변경과 BX 운영 부대에서 배려해 일정기간...","categories": ["Private"],
+        "tags": ["Private","ahg223"],
+        "url": "http://localhost:4000/private/Military/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "프로그래밍의 가치",
+        "excerpt":"컴퓨터 공학의 매력은 통신, 데이터, 자동화에 있다고 생각한다. 이러한 자동화 기술은 인류를 노동으로부터 해방시키고 있고, 플랫폼을 통해 생태계를 구축하기도 하였다. 그리고 이제는 데이터를 이용해 사회에서 당연히 받아들였던 인문학적 한계를 극복해나가고 있다. 예를 들자면 블록체인을 통해 인증에 있어 제3의 신뢰 보증 기관을 제외하는 DID가 그러하다. 각각의 공학 기술들은 급속한 발전을 이루며...","categories": ["Private"],
+        "tags": ["Private","ahg223"],
+        "url": "http://localhost:4000/private/private/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "Why Contributing",
+        "excerpt":"성장욕은 개인을 이기적으로 만든다 생각했지만, 나와 같은 범재들이 성장하기 위해서는 아이러니하게도 집단에 소속되어 집단을 발전시키는 것이 가장 좋은 방법이다. 나의 첫 팀 개발 프로젝트는 군전역 후 대학교 3학년이었다. 당시 개발자답지 못한 나의 팀 운영 때문에 팀원들은 git을 소개해주며 fork 기능에 담긴 오픈소스 협업 문화를 알려주었다. 배우고 난 뒤 미안함과 감사함에...","categories": ["Private"],
+        "tags": ["Private","ahg223"],
+        "url": "http://localhost:4000/private/private/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"}]
